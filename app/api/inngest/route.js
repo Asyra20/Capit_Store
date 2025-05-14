925e8e4edcfc57ec5e29e68437d6eec8a6bb6b46
Asyra20 <rasyraf7@gmail.com>
2025-05-14 20:43:34 +0700

    fix direktori

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -2,8 +2,11 @@ import { serve } from "inngest/next";
 import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
 
 // Create an API that serves zero functions
-export const { GET, POST, PUT } = serve(
-  inngest,
-  [syncUserCreation, syncUserUpdation, syncUserDeletion]
-);
-
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    syncUserCreation,
+    syncUserUpdation,
+    syncUserDeletion
+  ],
+});
