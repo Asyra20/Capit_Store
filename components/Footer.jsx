573ad2c53aed334dc839570dad6fde6060475b76
Khoniv <oniefzk11@gmail.com>
2025-05-16 21:23:14 +0700

    Header Slider

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,10 +9,7 @@ const Footer = () => {
         <div className="w-4/5">
           <Image className="w-28 md:w-32" src={assets.logo} alt="logo" />
           <p className="mt-6 text-sm">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
+            Capit Store is a sandal sales website that provides various models of contemporary sandals for men and women. Offering stylish designs, comfortable to wear, and suitable for various activities, Capit Store comes with affordable prices and fast service.
           </p>
         </div>
 
