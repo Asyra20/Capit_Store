e349ec3067aec59d68688f4e12768031fc8068ba
Asyra20 <rasyraf7@gmail.com>
2025-05-16 09:29:43 +0700

    order API

--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import connDB from "./db";
 import User from "@/models/User";
+import Order from "@/models/Order";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "Capit-Store" });
@@ -14,46 +15,76 @@ export const syncUserCreation = inngest.createFunction(
     event: "clerk/user.created",
   },
   async ({ event }) => {
-    const {id, first_name, last_name, email_addresses, image_url} = event.data
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
     const userData = {
-        _id:id,
-        email: email_addresses[0].email_address,
-        name: first_name + ' ' + last_name,
-        imageUrl: image_url
-    }
-    await connDB()
-    await User.create(userData)
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      imageUrl: image_url,
+    };
+    await connDB();
+    await User.create(userData);
   }
 );
 
 // Inngest Function to update database
 export const syncUserUpdation = inngest.createFunction(
-    {
-        id: 'update-user-from-clerk'
-    },
-    { event: 'clerk/user.updated'},
-    async ({event}) => {
-        const {id, first_name, last_name, email_addresses, image_url} = event.data
-        const userData = {
-            _id:id,
-            email: email_addresses[0].email_address,
-            name: first_name + ' ' + last_name,
-            imageUrl: image_url
-        }
-        await connDB()
-        await User.findByIdAndUpdate(id,userData)
-    }
-)
+  {
+    id: "update-user-from-clerk",
+  },
+  { event: "clerk/user.updated" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      imageUrl: image_url,
+    };
+    await connDB();
+    await User.findByIdAndUpdate(id, userData);
+  }
+);
 
 // Inngest Function to delete
 export const syncUserDeletion = inngest.createFunction(
-    {
-        id:'delete-user-with-clerk'
+  {
+    id: "delete-user-with-clerk",
+  },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    const { id } = event.data;
+    await connDB();
+    await User.findByIdAndDelete(id);
+  }
+);
+
+// Inngest to create user order in db
+export const createUserOrder = inngest.createFunction(
+  {
+    id: "create-user-order",
+    batchEvents: {
+      maxSize: 5,
+      timeout: "5s",
     },
-    { event:'clerk/user.deleted'},
-    async ({event}) => {
-        const {id} = event.data
-        await connDB()
-        await User.findByIdAndDelete(id)
-    }
-)
+  },
+  {
+    event: "order/created",
+  },
+  async ({events}) => {
+    const orders = events.map((event)=>{
+      return {
+        userId: event.data.userId,
+        items: event.data.items,
+        amount: event.data.amount,
+        address: event.data.address,
+        date: event.data.date
+      }
+    })
+    await connDB()
+    await Order.insertMany(orders)
+    return { success: true, process: orders.length};
+  }
+);
--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -64,6 +64,15 @@ export const AppContextProvider = (props) => {
       cartData[itemId] = 1;
     }
     setCartItems(cartData);
+    if (user) {
+      try {
+        const token = await getToken()
+        await axios.post('/api/cart/update', {cartData},{headers:{Authorization:`Bearer ${token}`}})
+        toast.success('item added to cart')
+      } catch (error) {
+        toast.error(error.message)
+      }
+    }
   };
 
   const updateCartQuantity = async (itemId, quantity) => {
@@ -74,6 +83,15 @@ export const AppContextProvider = (props) => {
       cartData[itemId] = quantity;
     }
     setCartItems(cartData);
+    if (user) {
+      try {
+        const token = await getToken()
+        await axios.post('/api/cart/update', {cartData},{headers:{Authorization:`Bearer ${token}`}})
+        toast.success('Cart Updated')
+      } catch (error) {
+        toast.error(error.message)
+      }
+    }
   };
 
   const getCartCount = () => {
